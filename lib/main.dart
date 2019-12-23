05928a2a6f51a3a87734b40d5959c6fa7d03ff74
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2019-12-23 22:09:33 +0700

    Scaffolding (app & body)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ void main() => runApp(MyFirstApp());
 
 class MyFirstApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'));
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text(
+            'Ad cillum veniam mollit excepteur labore excepteur ullamco consectetur voluptate occaecat labore culpa. Eu et enim aliquip in nisi nulla duis ex voluptate laboris est ex sint dolore. Consequat nulla enim qui aliqua cillum officia cillum aute ea consectetur. Qui elit do incididunt officia magna. In laborum esse veniam mollit et.'),
+      ),
+    );
   }
 }
